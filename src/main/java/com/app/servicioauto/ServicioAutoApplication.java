018fe5d8ce4088260bccef1c24938eb3562a3bf8
Kairos Jesus Adrian Diaz Orozco <nem.jadrian@coppel.com>
2023-07-17 11:25:05 -0700

    Inicializando proyecto

---
+++ src/main/java/com/app/servicioauto/ServicioAutoApplication.java
@@ -0,0 +1,13 @@
+package com.app.servicioauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioAutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioAutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/servicioauto/ServicioAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.servicioauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioAutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
